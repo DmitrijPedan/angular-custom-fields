100917eed50fe8ec6ebdeae21d58b8faadac076a
DmitrijPedan <dmitrijpedan84@gmail.com>
2022-05-22 13:27:02 +0300

    fixes

--- src/app/custom-fields/custom-fields.module.ts
+++ src/app/custom-fields/custom-fields.module.ts
@@ -7,21 +7,20 @@ import { CustomFieldService } from "./services/custom-field.service";
 import { GroupControlComponent } from './components/group-control/group-control.component';
 import { FieldFormComponent } from './components/field-form/field-form.component';
 import { CustomFieldsFormComponent } from './components/custom-fields-form/custom-fields-form.component';
-import { AddFieldButtonComponent } from './components/add-field-button/add-field-button.component';
 
 @NgModule({
   declarations: [
     GroupControlComponent,
     FieldFormComponent,
     CustomFieldsFormComponent,
-    AddFieldButtonComponent,
   ],
   exports: [
     CustomFieldsFormComponent
   ],
   imports: [
     CommonModule,
-    FormsModule, ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [CustomFieldService]
 })
