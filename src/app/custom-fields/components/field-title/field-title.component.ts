9d06940078edd59ae5cdbad84030f92d822ed34e
DmitrijPedan <dmitrijpedan84@gmail.com>
2022-05-22 18:29:46 +0300

    add type and label to field title

--- src/app/custom-fields/components/field-title/field-title.component.ts
+++ src/app/custom-fields/components/field-title/field-title.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {FieldType} from "../../interfaces/interfaces";
 
 @Component({
   selector: 'app-field-title',
   templateUrl: './field-title.component.html',
   styleUrls: ['./field-title.component.scss']
 })
-export class FieldTitleComponent implements OnInit {
+export class FieldTitleComponent implements OnInit, OnChanges {
 
   @Input() number: number = 0;
-  @Input() name: string = 'Field';
-  @Output() remove: EventEmitter<void> = new EventEmitter<void>();
-  @Output() addField: EventEmitter<void> = new EventEmitter<void>();
+  @Input() value: any;
+  public label: string = 'Field';
+  public type!: FieldType;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const value = changes?.value?.currentValue;
+    if (value && value.conditions) {
+      this.label = value.conditions.label;
+      this.type = value.conditions.type;
+    }
   }
 
 }
