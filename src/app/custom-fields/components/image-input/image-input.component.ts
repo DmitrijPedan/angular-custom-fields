cf604237626988152391c53be232eb9fbd7dd76d
DmitrijPedan <dmitrijpedan84@gmail.com>
2022-05-26 10:00:10 +0300

    fixes image input

--- src/app/custom-fields/components/image-input/image-input.component.ts
+++ src/app/custom-fields/components/image-input/image-input.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ImageInputComponent implements OnInit {
 
   @Input() imageSrc: string = '';
+  @Input() required = false;
   @Output() addImage: EventEmitter<void> = new EventEmitter<void>()
   @Output() removeImage: EventEmitter<void> = new EventEmitter<void>()
 
